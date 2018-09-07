39105d6ec9a13b3466eaea4d7639a38b2a988f4b
lxyer <lxyer1@gmail.com>
2018-09-07 14:43:24 +0800

    success

---
+++ src/main/java/com/leetcode/lxyer/answers/ReverseInteger.java
@@ -0,0 +1,31 @@
+package com.leetcode.lxyer.answers;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: lxyer
+ * Date: 07/09/2018
+ * Time: 14:41
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ * Input: 123
+ * Output: 321
+ *
+ * Example 2:
+ * Input: -123
+ * Output: -321
+ *
+ * Example 3:
+ * Input: 120
+ * Output: 21
+ */
+public class ReverseInteger{
+	public int reverse(int x) {
+		
+		return 1;
+	}
+}
