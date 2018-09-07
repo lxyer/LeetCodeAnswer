ed00b111b2a6092277a82a87368d746918c17787
lxyer <lxyer1@gmail.com>
2018-09-07 13:52:16 +0800

    Initial commit

---
+++ src/main/java/com/leetcode/lxyer/LxyerApplication.java
@@ -0,0 +1,12 @@
+package com.leetcode.lxyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxyerApplication{
+	
+	public static void main(String[] args){
+		SpringApplication.run(LxyerApplication.class,args);
+	}
+}
