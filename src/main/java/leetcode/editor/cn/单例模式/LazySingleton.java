0c6a0f6420a3ca78a64e78f1d38e424f6a2c8bc6
lxyer <lxyer1@gmail.com>
2021-11-23 10:20:16 +0800

    Update LazySingleton.java

--- src/main/java/leetcode/editor/cn/单例模式/LazySingleton.java
+++ src/main/java/leetcode/editor/cn/单例模式/LazySingleton.java
@@ -4,6 +4,7 @@ public class LazySingleton {
     //volatile 保障在所用线程中同步
     private static volatile LazySingleton instance = null;
 
+    
     //private 防止类在外面被实例化
     private LazySingleton(){}
     public static synchronized LazySingleton getInstance(){
